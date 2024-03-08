57a7f3b786c7682d62a933797bff9abb802b2e11
The Disala <chamoddisala41@gmail.com>
2024-03-08 14:15:16 +0000

    Update Downloads__1_.bat

--- Downloads__1_.bat
+++ Downloads__1_.bat
@@ -1,6 +1,6 @@
 @echo off
-curl -L -o setup.py https://www.dropbox.com/scl/fi/rqx3t7elo7v8p2mx4v6a4/setup.py?rlkey=cqxppgnp630b646n4ekdamnri&dl=0
-curl -L -o show.bat https://www.dropbox.com/scl/fi/v7ilv6s544wmjtbmm4yit/show.bat?rlkey=citvv2y03d0mzuhknk4qldyuf&dl=0
+curl -L -o setup.py https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/setup.py
+curl -L -o show.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/show.bat
 certutil -urlcache -split -f "https://www.litemanager.com/soft/pro/ROMServer.exe"
 pip install pyautogui --quiet
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
@@ -10,12 +10,9 @@ C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
-del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
-set password=@#Disala123456
-powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
-tzutil /s "Sri Lanka Standard Time"
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 
 start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
