07c03b433cade2246fbffe8ea147293682ee95cd
The Disala <chamoddisala41@gmail.com>
2024-03-08 14:42:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,37 +4,30 @@ import pyperclip
 
 # Define the coordinates and use the `actions` list
 actions = [
-    (713, 521, 2),  # click next
-    (452, 297, 2),  # additional
-    (626, 420, 2),  # change pass
-    (399, 255, 3),  # type pass
-    (398, 306, 2),  # type pass
-    (563, 600, 2),  # ok
-    (377, 299, 2),  # change tab
-    (557, 393, 3),  # right click (select all)
-    (540, 388, 4),  # right click (copy)
+    (704, 509, 2),  # click next
+    (450, 293, 10),  # additional
+    (628, 414, 2),  # change pass
+    (384, 253, 3),  # type pass
+    (384, 299, 2),  # type pass
+    (584, 595, 2),  # ok
+    (371, 295, 2),  # change tab
+    (532, 387, 3),  # right click (select all)
+    (575, 451, 4),  # right click (copy)
           ]
 
 # Wait for a few seconds to give time to focus on the target application
-time.sleep(2)
+time.sleep(20)
 password = "disalardp"
     
 for x, y, duration in actions:
-        if (x, y, duration) == (399, 255, 3):
+        if (x, y, duration) == (384, 253, 3):
             pag.click(x, y, duration=duration)
             pag.typewrite(password)
-        elif (x, y, duration) == (713, 521, 2):
-            pag.click(x, y, duration=duration)
-            time.sleep(10)
-        elif (x, y, duration) == (398, 306, 2):
+        elif (x, y, duration) == (384, 299, 2):
             pag.click(x, y, duration=duration)
             pag.typewrite(password)
-        elif (x, y, duration) == (540, 388, 4):
+        elif (x, y, duration) == (532, 387, 3):
             pag.rightClick(x, y, duration=duration)
-            pag.click(593, 446, duration=2) # click copy
-        elif (x, y, duration) == (557, 393, 3):
-            pag.rightClick(x, y, duration=duration)
-            pag.click(618, 527, duration=2) # click select all
         else:
             pag.click(x, y, duration=duration)
 
@@ -44,7 +37,7 @@ def save_echo_to_batch(file_path, echo_text):
 
 def save_command():
     clipboard_text = pyperclip.paste()
-    password_echo = 'Password : disalardp'  
+    password_echo = 'LiteManager Password : disalardp'  
     save_echo_to_batch('show.bat', f'LiteManager ID: {clipboard_text}')
     save_echo_to_batch('show.bat', password_echo)
 
