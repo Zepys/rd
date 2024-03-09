fc41c5b0f1c701e1e935343e39459e580808604b
The Disala <chamoddisala41@gmail.com>
2024-03-09 05:01:44 +0000

    Update show.bat

--- show.bat
+++ show.bat
@@ -1,5 +1,4 @@
 @echo off
-wall.bat
 start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
 python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
 python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
