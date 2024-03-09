c7a9d45c1fc97f72bfc06c244862443193757e67
The Disala <chamoddisala41@gmail.com>
2024-03-09 03:50:25 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,34 @@ import pyperclip
 
 # Define the coordinates and use the `actions` list
 actions = [
-    (704, 509, 2),  # click next
-    (450, 293, 10),  # additional
-    (628, 414, 2),  # change pass
-    (384, 253, 3),  # type pass
-    (384, 299, 2),  # type pass
-    (584, 595, 2),  # ok
-    (371, 295, 2),  # change tab
-    (532, 387, 3),  # right click (select all)
-    (575, 451, 4),  # right click (copy)
+    (610, 531, 3),  # next
+    (286, 459, 3),  # Accept terms
+    (610, 531, 3),  # next
+    (610, 531, 3),  # next
+    (610, 531, 3),  # next
+    (610, 531, 3),  # install
+    (387, 253, 10),  # type pass
+    (387, 298, 3),  # type pass
+    (564, 598, 3),  # ok
+    (610, 531, 2),  # next
+    (849, 746, 10),  # open litemanager
+    (522, 388, 3),  # click connect
+    (460, 321, 3),  # right click (select all)
+    (506, 387, 3),  # click copy
           ]
 
 # Wait for a few seconds to give time to focus on the target application
-time.sleep(20)
-password = "disalardp"
+time.sleep(10)
+password = "TheDisa1a"
     
 for x, y, duration in actions:
-        if (x, y, duration) == (384, 253, 3):
+        if (x, y, duration) == (387, 253, 10):
             pag.click(x, y, duration=duration)
             pag.typewrite(password)
-        elif (x, y, duration) == (384, 299, 2):
+        elif (x, y, duration) == (387, 298, 3):
             pag.click(x, y, duration=duration)
             pag.typewrite(password)
-        elif (x, y, duration) == (532, 387, 3):
+        elif (x, y, duration) == (460, 321, 3):
             pag.rightClick(x, y, duration=duration)
         else:
             pag.click(x, y, duration=duration)
@@ -37,7 +42,7 @@ def save_echo_to_batch(file_path, echo_text):
 
 def save_command():
     clipboard_text = pyperclip.paste()
-    password_echo = 'LiteManager Password : disalardp'  
+    password_echo = 'LiteManager Password : TheDisa1a'  
     save_echo_to_batch('show.bat', f'LiteManager ID: {clipboard_text}')
     save_echo_to_batch('show.bat', password_echo)
 
