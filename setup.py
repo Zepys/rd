4ea9ef8596ead53f4c06a0dd4098b1a30e9f900c
The Disala <chamoddisala41@gmail.com>
2024-03-15 10:28:54 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,20 @@ import subprocess
 
 # Define the coordinates and use the `actions` list
 actions = [
-    (610, 531, 3),  # next
-    (286, 459, 3),  # Accept terms
-    (610, 531, 3),  # next
-    (610, 531, 3),  # next
-    (610, 531, 3),  # next
-    (610, 531, 3),  # install
+    (610, 531, 4),  # next
+    (286, 459, 4),  # Accept terms
+    (610, 531, 4),  # next
+    (610, 531, 4),  # next
+    (610, 531, 4),  # next
+    (610, 531, 4),  # install
     (387, 253, 10),  # type pass
-    (387, 298, 3),  # type pass
-    (564, 598, 3),  # ok
-    (610, 531, 2),  # finish
+    (387, 298, 4),  # type pass
+    (564, 598, 4),  # ok
+    (610, 531, 4),  # finish
     (849, 746, 10),  # open litemanager
-    (522, 388, 3),  # click connect
-    (522, 388, 1),  # click connect
-    (460, 321, 3),  # right click (select all)
-    (506, 387, 3),  # click copy
+    (522, 388, 4),  # click connect
+    (460, 321, 4),  # right click (select all)
+    (506, 387, 4),  # click copy
 ]
 
 # Wait for a few seconds to give time to focus on the target application
@@ -30,12 +29,12 @@ for x, y, duration in actions:
     if (x, y, duration) == (387, 253, 10):
         pag.click(x, y, duration=duration)
         pag.typewrite(password)
-    elif (x, y, duration) == (387, 298, 3):
+    elif (x, y, duration) == (387, 298, 4):
         pag.click(x, y, duration=duration)
         pag.typewrite(password)
-    elif (x, y, duration) == (460, 321, 3):
+    elif (x, y, duration) == (460, 321, 4):
         pag.rightClick(x, y, duration=duration)
-    elif (x, y, duration) == (610, 531, 2):
+    elif (x, y, duration) == (610, 531, 4):
         pag.click(x, y, duration=duration)
         cmd = r'"C:\Program Files (x86)\LiteManager Pro - Server\ROMServer.exe" /start'
         subprocess.run(cmd, shell=True)
